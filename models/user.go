93596444e481cde2d106ba21f4fd94581bf82ec7
nadirbasalamah <nadir008basalamah@gmail.com>
2023-05-29 14:37:53 +0700

    fix(user): add unique constraint for user email

--- models/user.go
+++ models/user.go
@@ -12,6 +12,6 @@ type User struct {
 	UpdatedAt time.Time      `json:"updated_at"`
 	DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"index"`
 	Name      string         `json:"name"`
-	Email     string         `json:"email"`
+	Email     string         `json:"email" gorm:"unique"`
 	Password  string         `json:"password"`
 }
